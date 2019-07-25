9347651d602a443bc4097975a3c18042050c4ecb
JohnPank <pankevgenij@yandex.ru>
2019-07-25 11:30:08 +0300

    Chapter 6 complete

---
+++ chapter7/src/com/johnpank/chapter7/DBContract.java
@@ -0,0 +1,22 @@
+package com.johnpank.chapter7;
+
+public class DBContract {
+
+    public static final String DB_NAMES = "notebook";
+
+    //------users-------
+    public static final String USERS_TABLE = "users";
+    public static final String USER_ID= "id";
+    public static final String USER_LOGIN = "login";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_EMAIL = "email";
+    public static final String USER_BIRTH_DATE = "birthDate";
+    public static final String USER_REG_DATE = "regDate";
+
+    //------notes-------
+    public static final String NOTES_TABLE = "notes";
+    public static final String NOTE_ID = "id";
+    public static final String NOTE_USER_ID = "userId";
+    public static final String NOTE_CONTENT = "content";
+    public static final String NOTE_ADD_DATA = "addDate";
+}
---
+++ chapter7/src/com/johnpank/chapter7/mainClass.java
@@ -0,0 +1,7 @@
+package com.johnpank.chapter7;
+
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
